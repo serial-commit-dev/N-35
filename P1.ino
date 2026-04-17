7e12e2ae19447b4a78cbccbc2e2129a90f784718
Neeraj S <neerajms2009@gmail.com>
2026-04-17 12:33:21 +0530

    Added 'millis' for duration for each function

--- P1.ino
+++ P1.ino
@@ -9,11 +9,21 @@ Servo LE; //left_elevon
 Servo RE; //right_elevon
 
 const int motorPin = 5;
-const int left_elevon = 4;
-const int right_elevon = 7;
+const int left_elevon = 6;
+const int right_elevon = 9;
 
 int LE_angle = 0;
 int RE_angle = 0;
+
+unsigned long = previous_millis = 0; //Estimated time for to take-off
+const long intervals = 12000;
+
+unsigned long = previous_millis_DOWN = 0;
+const long Intervals_DOWN = 4333;
+
+unsigned long previous_millis_right = 0;
+const long interval_right = 20000;
+
 
 void setup(){
   Serial.begin(9600);
@@ -31,22 +41,52 @@ void loop(){
   
   digitalWrite(motorPin, true);
   while(digitalWrite(motorPin, true)){ //Initializes Motor (Sucks air in from both the Inlets)
-    //After x time
-    soft_Up();
+    
+    
+    
+    unsigned long current_millis = millis();  //After x time
+    if(current_millis - previous_millis >= intervals){
+      previous_millis = current_millis;
+      
+      soft_Up() //action
+    }
     //after the jet rises
     Up();
-    //after x time
-    soft_down();
-    //after 5 seconds
-    soft_down(); //the jet is now travelling horizontally without the nose pointing upward
+     //after x time
+   
+    
+    unsigned long current_millis_DOWN = millis();
+    if(current_millis_DOWN - previous_millis_DOWN >= Interval_DOWN){
+      previous_millis_DOWN = current_millis_DOWN;
+      
+      soft_down() //the jet is now travelling horizontally without the nose pointing upward
+        
+    }
+     
+   
+    soft_right();
+    
+    current_millis_right = millis();
+    if(current_millis_right - previous_millis_right >= interval_right){
+      previous_millis_right = current_millis_right;
+       
+    } 
 
-    //turning Triangle
-    soft_right()
-    //After 20 seconds
     soft_left();
     //After 20 seconds
+    current_millis_left = millis();
+    if(current_millis_left - previous_millis_left >= interval_left){
+      previous_millis_left = current_millis_left;
+       
+    } 
+    
     soft_left();
     //after another 20 seconds is over, by that time, the jet would've drawn a triangle
+    current_millis_right = millis();
+    if(current_millis_final_left - previous_millis_right >= interval_final_left){
+      previous_millis_final_left = current_millis_final_left;
+       
+    } 
   }
 
 
