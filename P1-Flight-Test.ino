c55f4fd65c6f00113930393e8b23de49e84d1e8c
Neeraj S <neerajms2009@gmail.com>
2026-04-18 10:13:37 +0530

    Fixing 'Nested Issue'

--- P1-Flight-Test.ino
+++ P1-Flight-Test.ino
@@ -1,4 +1,5 @@
 //phase-1 Take-Off
+//phase-2 Touch-Down
 #include <Servo.h>
 
 Servo LE;
@@ -11,11 +12,14 @@ const int right_elevon = 9;
 int LE_angle = 0;
 int RE_angle = 0;
 
-unsigned long = previous_millis = 0; //Estimated time for to ground-roll
-const long interval = 12000;
+unsigned long = previous_millis = 0; //Estimated time for to Ground-roll
+const long interval = 17000;
 
-unsigned long previous_takeOff_millis = 0; //
-const long interval = 12000;
+const long takeOff_interval = 27000; //Estimated time for soft-take-off
+const long Full_takeOff_interval = 42000; //Estimated time for Complete Take-Off!!
+
+const long soft_touchDown_interval = 54000; //Estimated time for soft touch down
+const long complete_touchDown_interval = 67000; //Estimated time for Landing
 
 void setup(){
   Serial.begin(9600);
@@ -30,13 +34,53 @@ void setup(){
 }
 
 void loop(){
+  ///////////////////////////////////////////!!!Take-Off Phase!!!///////////////////////////////////////////////////////
   digitalWrite(motorPin, true);
 
-  current_millis = millis();
-  if(current_millis - previous_millis >= interval){
-    previous_millis = current_millis;
-
+  //Ground-Roll for 17s before take-off
+  current_millis = millis();//the stopwatch
+  
+  if(current_millis - interval){
      digitalWrite(motorPin, true);
+
+    //Soft-Take-Off 
+    //Duration: 10s
+    takeOff_millis = millis();//the stopwatch
+    if(previous_millis - takeOff_millis >= takeOff_interval){
+      previous_millis = takeOff_millis;
+      
+      soft_up(); 
+
+      //Complete Take-Off!!
+      //Duration: 15s
+      Full_takeOff_millis = millis();
+      if(previous_millis - Full_takeOff_millis >= Full_takeOff_interval){
+        previous_millis = Full_takeOff_millis;
+
+        Up()
+
+           //////////////////////////////////////////////Touch-Down Phase!!///////////////////////////////////////////////////
+          soft_touchDown = millis();// the stopwatch
+          if(previous_millis - soft_tochDown >= soft_touchDown_interval){
+            previous_millis = soft_touchDown;
+
+            soft_down()
+
+              //Complete Touch Down;
+              complete_tocuhDown = millis();//the stopwatch
+              if(previous_millis - complete_tocuhDown >= complete_touchDown_interval){
+                previous_millis = complete_touchDown;
+
+                down()
+
+                       ////////////////////////End Of Flight Test/////////////////////////////
+              }
+          }
+      }
+       
+      
+    }
+    
   }
 
   
