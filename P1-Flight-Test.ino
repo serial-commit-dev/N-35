89230e0e62baa7d176c6644cdc8ff1e3179d9383
Neeraj S <neerajms2009@gmail.com>
2026-05-06 17:03:39 +0530

    The Complete Software

--- P1-Flight-Test.ino
+++ P1-Flight-Test.ino
@@ -5,7 +5,7 @@ Servo RE;
 
 const int motor_pin = 5;
 const int left_elevon = 6;
-const int rigth_elevon = 9;
+const int right_elevon = 9;
 
 int leftElevon_State = 90;
 int rightElevon_State = 90;
@@ -21,13 +21,13 @@ void setup() {
   RE.write(rightElevon_State);
 }
 
-unsigned long previous_millis = 0;
+unsigned long current_millis = 0;
 const long interval = 5583;
 
-const long pitch_up_interval = 7483;         //1.9s for picthing up
-const long neutral_interval = 8383;    // 900ms neutral
-const long kill_motor = 9653;       //1.27s   
-const long pitch_down_interval = 14013;     //4.36s     
+const long pitch_up_interval = 7483;  //1.9s for picthing up
+const long neutral_interval = 8383;  // 900ms neutral
+const long kill_motor = 9653;  //1.27s   
+const long pitch_down_interval = 14013;  //4.36s     
   
 
 
@@ -36,37 +36,26 @@ void loop() {
   
   //Ground-Roll
   if (current_millis >= interval){
-    previous_millis = current_millis;
-
     digitalWrite(motor_pin, true);
-
-    pitchUP_millis = millis();
-    if(current_millis - pitchUP_millis >= pitch_up_interval){
-      prvious_millis = pitchUP_millis;
-      pitch_up() //Reaches upto 20.1m/s
-    }
-    
-    
-    stayNeutral_millis = millis();
-    if(current_millis - stayNeutral_millis >= neutral_interval){
-      previous_millis = stayNeutral_millis;
-      neutral_position() //to let the jet feel air
-    }
-    
-
-    motor_kill_millis = millis();
-    if(stayNeutral_millis - motor_kill_millis  >= kill_motor){
-      digitalWrite(motor_pin, false); //The jet should reach its peak velocity by, here (24.7m/s)
-      
-      
-      pitch_down() 
-    }
   }
 
-  
+    else if(current_millis >= pitch_up_interval){
+      pitch_up(); //Reaches upto 20.1m/s
+    }
+    
+    else if(current_millis >= neutral_interval){
+      neutral_position(); //to let the jet feel air
+    }
 
+    else if(current_millis >= kill_motor){
+      digitalWrite(motor_pin, false); //The jet should reach its peak velocity by, here (24.7m/s)
+    }  
+      
+    else if(current_millis >= pitch_down_interval){
+      pitch_down(); 
+    }  
+      
 }
-
 
 
 
@@ -76,7 +65,7 @@ void pitch_up(){
   RE.write(98);
 }
 
-void pitch_down();
+void pitch_down(){
   LE.write(82);// -8 degree descend
   RE.write(82);
 }
